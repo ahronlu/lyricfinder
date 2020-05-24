1a6347a6b158ad4c19788e724a06eaa8eb714ef1
koftov <ahronlu@gmail.com>
2020-05-24 18:48:30 +0300

    deploy

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Layout/Navbar';
+import Index from './components/Layout/Index';
+import Lyrics from './components/tracks/Lyrics';
+
+import { Provider } from './context';
+
+function App() {
+  return (
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
